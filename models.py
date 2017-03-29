4dd559f04824b46d4f466c942b864d871694123d
Yilia05 <532104074@qq.com>
2017-03-29 20:53:12 +0800

    loginpage

--- models.py
+++ models.py
@@ -44,13 +44,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(32))
+    salt = db.Column(db.String(32))
     head_url = db.Column(db.String(256))
     head_url2 = db.Column(db.String(256))
     images = db.relationship('Image',backref='user',lazy='dynamic')
 
-    def __init__(self, username, password):
+    def __init__(self, username, password,salt=''):
         self.username = username
         self.password = password
+        self.salt = salt
         self.head_url = 'http://image.nowcoder.com/head/' + str(random.randint(0, 1000)) + 'm.png'
 
     def __repr__(self):
--- views.py
+++ views.py
@@ -1,8 +1,9 @@
 # -*- encoding=UTF-8 -*-
 
-from  Yistagram import app
+from  Yistagram import app,db
 from  models import Image,User
-from  flask import render_template,redirect
+from  flask import render_template,redirect,request,flash,get_flashed_messages
+import random,hashlib
 
 @app.route('/')
 def index():
@@ -22,3 +23,41 @@ def profile(user_id):
     if user == None:
         return redirect('/')
     return render_template('/profile.html', user = user)
+
+@app.route('/reloginpage')
+def regloginpage():
+    return render_template('/login.html')
+
+def redirect_with_msg(target,msg,category):
+    if msg != None:
+        flash(msg,category=category)
+    return redirect(target)
+
+@app.route('/reg/', methods={'post', 'get'})
+def reg():
+    #request.args url里的参数
+    #request.form post里的参数，value自动选择
+    username = request.values.get('username').strip()
+    password = request.values.get('password').strip()
+
+    if username == '' or password == '':
+        redirect_with_msg('/reloginpage/',u'用户名或密码为空','relogin')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user != None:
+        redirect_with_msg('/reloginpage/',u'用户名已存在','relogin')
+
+    #更多判断
+
+    salt = '.'.join(random.sample('0123456789abcdefghijklmnopqrstABCDEFGHIJKLMNOPQRSTUVWXYZ',10))
+    m = hashlib.md5()
+    m.update(password+salt)
+    password = m.hexdigest()
+    user = User(username, password, salt)
+    db.session.add(user)
+    db.session.commit()
+
+    return  redirect('/')
+
+
