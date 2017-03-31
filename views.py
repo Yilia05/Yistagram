8b5c1145d3194aa899e291247c041ac27d44246a
Yilia05 <532104074@qq.com>
2017-04-01 00:13:50 +0800

    loginpage update

--- views.py
+++ views.py
@@ -1,37 +1,70 @@
 # -*- encoding=UTF-8 -*-
 
-from  Yistagram import app,db
-from  models import Image,User
-from  flask import render_template,redirect,request,flash,get_flashed_messages
-import random,hashlib
+from Yistagram import app, db
+from models import Image, User
+from flask import render_template, redirect, request, flash, get_flashed_messages
+import random, hashlib
+from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route('/')
 def index():
     images = Image.query.order_by('id desc').limit(10).all()
-    return render_template('index.html',images=images)
+    return render_template('index.html', images=images)
 
 @app.route('/image/<int:image_id>/')
 def image(image_id):
     image = Image.query.get(image_id)
     if image == None:
         return redirect('/')
-    return  render_template('pageDetail.html', image = image)
+    return  render_template('pageDetail.html', image=image)
 
 @app.route('/profile/<int:user_id>')
+@login_required
 def profile(user_id):
     user = User.query.get(user_id)
     if user == None:
         return redirect('/')
-    return render_template('/profile.html', user = user)
+    return render_template('/profile.html', user=user)
 
-@app.route('/reloginpage')
-def regloginpage():
-    return render_template('/login.html')
+@app.route('/reloginpage/')
+def reloginpage():
+    if current_user.is_authenticated:
+        return redirect('/')
+    msg = ''
+    for m in get_flashed_messages(with_categories=False, category_filter=['relogin']):
+        msg = msg + m
+    return render_template('login.html', msg=msg, next=request.values.get('next'))
 
 def redirect_with_msg(target,msg,category):
     if msg != None:
-        flash(msg,category=category)
+        flash(msg, category=category)
     return redirect(target)
+
+
+@app.route('/login/', methods={'get', 'post'})
+def login():
+    username = request.values.get('username').strip()
+    password = request.values.get('password').strip()
+    # 校验
+    user = User.query.filter_by(username=username).first()
+    if username == '' or password == '':
+        return redirect_with_msg('/reloginpage', u'用户名和密码不能为空', 'relogin')
+
+    if user == None:
+        return redirect_with_msg('/reloginpage', u'用户名不存在', 'relogin')
+
+    m = hashlib.md5()
+    m.update(password + user.salt)
+    if m.hexdigest() != user.password:
+        return redirect_with_msg('/reloginpage', u'密码错误', 'relogin')
+
+    login_user(user)
+
+    next = request.values.get('next')
+    if next != None and next.startswith('/') > 0:
+        return redirect(next)
+
+    return redirect('/')
 
 @app.route('/reg/', methods={'post', 'get'})
 def reg():
@@ -39,14 +72,13 @@ def reg():
     #request.form post里的参数，value自动选择
     username = request.values.get('username').strip()
     password = request.values.get('password').strip()
-
-    if username == '' or password == '':
-        redirect_with_msg('/reloginpage/',u'用户名或密码为空','relogin')
-
     user = User.query.filter_by(username=username).first()
 
+    if username == '' or password == '':
+        return redirect_with_msg('/reloginpage/', u'用户名或密码为空','relogin')
+
     if user != None:
-        redirect_with_msg('/reloginpage/',u'用户名已存在','relogin')
+        return redirect_with_msg('/reloginpage/', u'用户名已存在','relogin')
 
     #更多判断
 
@@ -54,10 +86,19 @@ def reg():
     m = hashlib.md5()
     m.update(password+salt)
     password = m.hexdigest()
+
+    #提交新注册到user数据库
     user = User(username, password, salt)
     db.session.add(user)
     db.session.commit()
 
-    return  redirect('/')
+    login_user(user)
+
+    return redirect('/')
+
+@app.route('/logout/')
+def logout():
+    logout_user()
+    return redirect('/')
 
 
